fdf4e7ac80129d631c6ed29e7217073c15f9b4fe
Aitula-svg <aitu.ru00@gmail.com>
2024-07-24 17:30:35 +0300

    Projeck

---
+++ script.js
@@ -0,0 +1,123 @@
+// Задание 3
+
+
+// alert ("Вы успешно зарегистрировались!");
+
+// let reboot = confirm("При перезарузке сайта,введенные данные на форме будут очищены,вы уверены что хотите перезагрузить?");
+// console.log(reboot);
+
+// let username = prompt("Введите ваше имя");
+// console.log(username);
+
+
+// Задание 2
+
+// var a = 4,
+// i = 0,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+
+// var a = 4,
+// i = 1,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+// var a = 4,
+// i = 2,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+
+// var a = 4,
+// i = 3,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+
+// var a = 4,
+// i = 4,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+
+// var a = 4,
+// i = 5,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+
+// var a = 4,
+// i = 6,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+
+// var a = 4,
+// i = 7,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+
+// var a = 4,
+// i = 8,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+
+// var a = 4,
+// i = 9,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+
+// var a = 4,
+// i = 10,
+// x = a*i;
+// console.log(`${a} * ${i} = ${x}`);
+
+
+// Задание 3
+
+
+// let text1 = "Экзамендин катышуучулары";
+// let text2 = "жана результаттары";
+
+// let resulttext2 = `${text1} ${text2};
+// console.log{resulttext2}`;
+
+
+
+
+// let UserName = "1.Nooruz -";
+// let firstUserPoint = "97";
+// let firsttext = "из 100 баллов"
+
+
+
+// let result = `${UserName}  ${firstUserPoint} ${firsttext}`;
+ 
+// console.log(result);
+
+// let secondUserName = "2.Zhakshylyk -";
+// let secondUserPoint = "89";
+// let secondtext = "из 100 баллов"
+
+
+
+// let resultsecondUserPoint = `${secondUserName} ${secondUserPoint} ${secondtext}`
+// console.log(resultsecondUserPoint);
+
+// let thirdUserName = "3.Alisher -";
+// let thirdUserPoint = "75";
+// let thirdtext = "из 100 баллов"
+
+
+
+// let resultthirdUserPoint = `${thirdUserName} ${thirdUserPoint} ${thirdtext}`
+// console.log(resultthirdUserPoint);
+
+// let fourthUserName = "4.Elizar -";
+// let fourthUserPoint = "90";
+// let fourthtext = "из 100 баллов"
+
+
+
+// let resultfourthUserPoint = `${fourthUserName} ${fourthUserPoint} ${fourthtext}`;
+// console.log(resultfourthUserPoint);
+
+// let examen1 = "Экзамендин орточо балы:";
+// let examen2 = "87.75 балл";
+
+// let resultexamen2 =`${examen1} ${examen2}`;
+// console.log(resultexamen2);
